a1d107bb59cd75026f429ac2676649b14ee95a04
Zyaire Bayne <Zyaireb@vt.edu>
2024-02-07 01:14:03 -0500

    combined site data, with site name added

--- cap_Z.R
+++ cap_Z.R
@@ -1,8 +1,30 @@
-library(tidyverse)
 
+
+#call packages
+library(tidyverse)
+library(lubridate) 
+
+#Rename sites
 Site_A4 <- DATA_TS_A4_20240106
 Site_C3 <- DATA_TS_C3_20240106
 Site_D2 <- DATA_TS_A4_20240106
 Site_E1 <- DATA_TS_E1_20240106
 
-Site_A4 
+
+#Give each site it's own identifying column
+Site_A4 <- Site_A4 |>
+  mutate(Site = "A4")
+
+Site_C3 <- Site_C3 |>
+  mutate(Site = "C3")
+
+Site_D2 <- Site_D2 |>
+  mutate(Site = "D2")
+
+Site_E1 <- Site_E1 |>
+  mutate(Site = "E1")
+
+tot_site <- bind_rows(Site_A4, Site_C3, Site_D2, Site_E1)
+
+tot_site |>
+  distinct(Site)
