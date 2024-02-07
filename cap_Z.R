7a4ffe4962bf109f2e06dc4daf6a9b36fa89917e
Zyaire Bayne <Zyaireb@vt.edu>
2024-02-07 02:03:12 -0500

    ATTEMPT TO CHANGE DATETIME

--- cap_Z.R
+++ cap_Z.R
@@ -3,6 +3,14 @@
 #call packages
 library(tidyverse)
 library(lubridate) 
+library(readr)
+
+#read sites
+DATA_TS_A4_20240106 <- read_csv("Data/Timeseries_Data/DATA_TS_A4_20240106.csv")
+DATA_TS_C3_20240106 <- read_csv("Data/Timeseries_Data/DATA_TS_C3_20240106.csv")
+DATA_TS_D2_20240106 <- read_csv("Data/Timeseries_Data/DATA_TS_D2_20240106.csv")
+DATA_TS_E1_20240106 <- read_csv("Data/Timeseries_Data/DATA_TS_E1_20240106.csv")
+
 
 #Rename sites
 Site_A4 <- DATA_TS_A4_20240106
@@ -26,5 +34,19 @@ Site_E1 <- Site_E1 |>
 
 tot_site <- bind_rows(Site_A4, Site_C3, Site_D2, Site_E1)
 
+#test sites are in one df
 tot_site |>
   distinct(Site)
+
+
+
+                #tot_site1 <- tot_site |>
+               # mutate(Year = year(timestamp))
+                  #dates <- as.Date(tot_site$timestamp, format = "%d-%m-%Y")
+                  #tot_site$timestamp <- format(dates, "%Y-%m-%d")
+
+
+#tttt_sites <- parse_datetime(tot_site, format = "", na = na, local = default_locale(), trim_ws =  TRUE)
+
+
+ggplot(tot_site, aes(x = ))
