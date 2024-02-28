3a5aa5bb4ef0361685cd419bec917d8b60fb4bce
MicahAxford15 <micahaxford15@vt.edu>
2024-02-28 11:22:51 -0500

    AddingRasterMicah

--- rasterVisTesting.R
+++ rasterVisTesting.R
@@ -3,11 +3,13 @@ library(leaflet)
 raster_data <- rast("/Users/micahaxford/Desktop/CapstoneProjects/Data/Rasters/SnowChange_11_15_2_2.tif")
 
 #raster_ <- as.matrix(raster_data)
+palette1 <- colorNumeric(palette = 'Y10rRd', domain = values(raster_data))
 
 leaflet(raster_data) |> 
-  addTiles()
+  addTiles() |> 
+  addRasterImage(raster_data, colors = palette1 , opacity = 0.8)
 
-map <- fitBounds(map,43.96, -71.69, 43.91, -71.81)
+#map <- fitBounds(map,43.96, -71.69, 43.91, -71.81)
 
 map
 
