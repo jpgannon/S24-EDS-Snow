eb26948a7e11f4a3c891531d36b67d327d60f46d
Gabriel Escobar <gabrielse@vt.edu>
2024-02-28 11:23:30 -0500

    Test

--- app.R
+++ app.R
@@ -14,17 +14,15 @@ library(shinycssloaders)
 library(shinythemes)
 library(SwimmeR)
 library(terra)
-#library(raster)
-library(jsonlite)
-library(sp)
-library(sf)
-library(plotly)
+library(leaflet)
 
 #Reading in Data
 dataSites <- read.csv("averages_by_hour_allsites_1.csv")
 sites <- ordered(dataSites$Site_Name, levels = c("A4", "C3", "D2", "E1"))
 fiftystatesCAN <- read.csv("fiftystatesCAN.csv") #From https://github.com/gpilgrim2670/SwimMap/tree/master repo
 region <- fiftystatesCAN %>% filter(GeoRegion == "NewEngland")
+
+raster_data <- rast("Data/Rasters/SnowChange_11_15_2_2.tif")
 
 #shapefile <- readShapeSpatial("Data/Shapefiles/hbef_ws3_siteLocations/hbef_23-24_UAS_SiteLocations.shp")
 #shapefile_df <- fortify(shapefile)
@@ -124,34 +122,9 @@ ui <- fluidPage(
     
     #Map display
     output$scatterplotFinder <- renderPlot({
-      isolate({
-        ggplot() +
-          #Displays New England Shape on Map
-          geom_polygon(data = region, aes(x = long, y = lat, group = group), color = "white", fill = "grey") +
-          #geom_raster(data = raster_df, aes(x = x, y = y, fill = value)) + scale_fill_gradientn(colours = terrain.colors(10)) +
-          #(geom_sf(data = shapefile, aes(fill = "red")) + coord_sf()) +
-          #geom_polygon(data = shapefile_df, aes(x = longitude, y = latitude, group = group), color = "black", fill = "white") + 
-        
-          #Display sites
-          #geom_point(data = region, aes(x = long, y = lat, alpha = 0.8)) + 
-          
-          coord_quickmap() +
-          guides(fill = "none") +
-          theme_void() +
-          theme(axis.text = element_blank(), axis.ticks = element_blank()) +
-          theme(plot.title = element_text(hjust=0.5, face = "bold")) +
-          theme(plot.background = element_rect(fill = "white"), plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm")) +
-          guides(alpha = "none") +
-          theme(legend.text = element_text(size = 12),
-                legend.title = element_text(size = 15)) +
-          theme(plot.background = element_rect(
-            color = "white"
-          ))
-        
-        
-      })
-      
-      
+      leaflet(raster_data) %>%
+        addTiles() %>%
+        setView(lng = -74.0060, lat = 40.7128, zoom = 10)
       
     })
     
